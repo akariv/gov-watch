dfce1e55787508146e19713bf8dcfb68e96a5aec
Adam Kariv <adam.kariv@gmail.com>
2012-07-10 23:11:44 +0300

    ++

--- src/static/js/gov-watch.js
+++ src/static/js/gov-watch.js
@@ -1232,14 +1232,14 @@
       })(function() {
         try {
           current_version = localStorage.version;
-          localStorage.version = JSON.stringify(version);
           if (current_version && version === JSON.parse(current_version)) {
             loaded_data = JSON.parse(localStorage.data);
             all_books = JSON.parse(localStorage.all_books);
             all_tags = JSON.parse(localStorage.all_tags);
             all_people = JSON.parse(localStorage.all_people);
             all_subjects = JSON.parse(localStorage.all_subjects);
-            return process_data();
+            process_data();
+            return localStorage.version = JSON.stringify(version);
           } else {
             console.log("wrong version " + current_version + " != " + version);
             return load_data();
