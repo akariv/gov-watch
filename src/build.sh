b9d24f4fbead176e996d2cbd98a510e072c74358
mushon <mushon@shual.com>
2012-07-12 22:59:06 +0300

    debugging

--- src/build.sh
+++ src/build.sh
@@ -10,7 +10,7 @@ lessc --compress less/style.less > static/css/min.css
 echo Running ICED coffeescript
 iced -I inline -c -o static/js/ *.iced
 echo Concatenating JS files
-cat static/js/jquery.min.js static/bootstrap/js/bootstrap.min.js  static/js/jquery.isotope.min.js static/js/jquery.scrollintoview.min.js static/js/jquery.tinysort.min.js  > static/js/_min.js
+cat static/js/jquery.min.js static/bootstrap/js/bootstrap.min.js  static/js/jquery.isotope.min.js static/js/jquery.inview.min.js static/js/jquery.scrollintoview.min.js static/js/jquery.tinysort.min.js  > static/js/_min.js
 cat static/js/mustache.js static/js/gov-watch.js > static/js/_max.js
 echo Compressing JS
 cp static/js/{_,}min.js 
